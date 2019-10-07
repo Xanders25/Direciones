20e14cce10da7fc50d67eb70c06da9d5bb43b203
Xanders25 <xanders366@gmail.com>
2019-10-07 17:02:12 +0200

    variables

---
+++ 2a.php
@@ -0,0 +1,18 @@
+<html>
+<body>
+<?php
+// php - Variables para sudar sangre
+ 
+$n1 = 2025;
+$n2 = "41";
+ 
+echo "Los numeros que usare son ", $n1, " y ", $n2, ".<br />";
+echo "La Suma: ", $n1+$n2,"<br />";
+echo "Resta: ", $n1-$n2,"<br />";
+echo "Multiplicacion: ", $n1*$n2,"<br />";
+echo "Division: ", $n1/$n2,"<br />";
+printf("Division sin decimales: %d<br />", $n1/$n2);
+echo "Resto de la division: ", $n1 % $n2, "<br />";
+?>
+</body>
+</html>
