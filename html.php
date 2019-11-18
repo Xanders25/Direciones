a1969255c5852ea0da8b4e634902a06e1dbbd422
Xanders25 <xanders366@gmail.com>
2019-11-18 16:26:12 +0100

    pagina web corregida+ç

---
+++ html.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Articulo - Valencia</title>
+</head>
+<body bgcolor="#E6E6FA">
+
+<h1>Estalla el 'caso Guedes' en el Valencia</h1>
+
+<h2>El luso comunica al club que se va a buscar una nueva opinión médica para solventar su lesión</h2>
+<h3>No tiene fé de que se solucione su problema.</h3>
+<h4>Se queda sin opciones</h4>
+<center>
+<img src="vlc.jpg" alt="Guedes en lesión"/>
+</center>
+<p><u>Gonçalo Guedes</u> ha dicho basta. El extremo portugués ha decidido que ya no espera más. 
+Por ese motivo <b>ha comunicado al <font size="1">club</font> que se marcha</b> a buscar una nueva opinión médica por
+ la lesión en el tobillo (tal y como desvela el programa Tribuna Deportiva y que ha 
+ confirmado MARCA) que se produjo el día después de volver de <font color="red">Portugal</font> en el anterior parón.</p>
+
+<p>Le duele, no le aciertan el diagnóstico y da muestras de cojera como se pudo ver en la presencia de Guedes en el palco del Ciutat para ver el Levante-Barcelona. 
+El jugador de 22 años iba cojo con lo que ese problema no se ha solventado. </p>
+
+<p>El futbolista ha acordado con sus agentes la necesidad de buscar una solución a sus 
+dolencias que no remiten fruto de los problemas internos que hay en los servicios médicos.</p>
+
+<br/>
+
+<h1>Datos para administrar informacion deportiva</h1>
+<p>*Orientativa</p>
+
+<form>
+  <p>Nombre: <input type="text" name="nombre" size="40"/></p>
+  <p>Año de nacimiento: <input type="number" name="año" min="1900"/></p>
+  <p>Sexo:
+    <input type="radio" name="hm" value="h"/> Hombre
+    <input type="radio" name="hm" value="m"/> Mujer
+  </p>
+  <p>
+    <input type="submit" value="Enviar"/>
+    <input type="reset" value="Borrar"/>
+  </p>
+</form>
+
+<h1>Autonomias bañadas por el cantabrico</h1>
+
+<table border="">
+  <tr>
+    <th>Norte</th>
+    <th>Mas al norte</th>
+  </tr>
+  <tr>
+    <td>Cantabria</td>
+    <td>Asturias</td>
+  </tr>
+  <tr>
+    <td>Eve</td>
+    <td>Jackson</td>
+  </tr>
+</table> 
+
+</body>
+</html> 
